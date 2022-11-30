59760a9f454f5a37c689f2a65a827c5c2b464d6b
Soham Jog <jogsoham2003@gmail.com>
2022-11-30 15:03:20 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ useEffect(() => {
       
       <div className='snap-y h-screen w-screen overflow-scroll'>
       
+      {/* SLIDE 1*/}
+
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
           <div className=' px-40 flex flex-col items-center justify-center duration-500'>
@@ -36,6 +38,24 @@ useEffect(() => {
           </div>  
 
       </div>
+
+
+      {/* SLIDE 1*/}
+
+      <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
+
+          <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal'> 
+            Add zk-verifiable AI inference to your stack</h1>
+            
+            <p2 className='text-xl text-white font-bold mb-10'>The Graphiti Protocol Fast. Scalable. Intelligent</p2>
+          </div>  
+
+      </div>
+
+
+      {/* FORM SLIDE */}
+
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
       <div className='relative'>
           <div className='absolute inset-0 bg-purple-600 rounded-lg blur'></div>
@@ -45,8 +65,6 @@ useEffect(() => {
 
 
 
-      <div className='snap-start bg-gradient-to-r from-white to-purple-400 w-screen h-screen flex items-center justify-center text-8xl'>3</div>
-      <div className='snap-start bg-fuchsia-200 w-screen h-screen flex items-center justify-center text-8xl'>4</div>
 
       
 
