2b1680a208c9adb8179e64336749450e0aab22fc
Soham Jog <jogsoham2003@gmail.com>
2022-11-30 15:24:07 -0500

    slide 3 not neat

--- src/App.js
+++ src/App.js
@@ -31,24 +31,36 @@ useEffect(() => {
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
           <div className=' px-40 flex flex-col items-center justify-center duration-500'>
-            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal'> 
+            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
             Add zk-verifiable AI inference to your stack</h1>
             
-            <p2 className='text-xl text-white font-bold mb-10'>The Graphiti Protocol Fast. Scalable. Intelligent</p2>
+            <h2 className='text-xl text-white font-bold mb-10'>The Graphiti Protocol Fast. Scalable. Intelligent</h2>
           </div>  
 
       </div>
 
 
-      {/* SLIDE 1*/}
+      {/* SLIDE 2*/}
 
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
           <div className=' px-40 flex flex-col items-center justify-center duration-500'>
-            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal'> 
-            Add zk-verifiable AI inference to your stack</h1>
+            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
+            Connect powerful machine learning models to your smart contracts</h1>
+          </div>  
+
+      </div>
+
+
+      {/* SLIDE 3*/}
+
+      <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
+
+          <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
+            Offchain computation, on chain verification</h1>
             
-            <p2 className='text-xl text-white font-bold mb-10'>The Graphiti Protocol Fast. Scalable. Intelligent</p2>
+            <h2 className='text-xl text-white font-bold mb-10'>Leveraging zero knowledge proofs to provide proof of execution and proof of accuracy</h2>
           </div>  
 
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import logo from '../assets/logo.jpeg';
+
 
 
 const NavBar = () => {
@@ -10,13 +10,13 @@ const NavBar = () => {
       <nav className="relative bg-black px-4 py-2.5 fixed w-full h-20 z-50 top-0 left-0 flex items-center justify-between">
 
           <div className="flex items-center">
-              <img src = {logo} alt = {''}/>
+              <h1 className='text-purple-400 text-8xl text-mono text-bold'>G</h1>
           </div>
       <div className='flex items-center justify-between'>
-        <h1 className="px-2 flex items-center text-xl text-white "><a href="/" >Home</a></h1>
-        <h1 className="px-2 flex items-center text-xl text-white "><a href = "/" >Team</a></h1>
-        <h1 className="px-2 flex items-center text-xl text-white "><a href="/" >Use Cases</a></h1>
-        <h1 className="px-2 flex items-center text-xl text-white "><a href="/" >Roadmap</a></h1>
+        <h1 className="px-6 flex items-center text-xl text-white "><a href="/" >Home</a></h1>
+        <h1 className="px-6 flex items-center text-xl text-white "><a href = "/" >Team</a></h1>
+        <h1 className="px-6 flex items-center text-xl text-white "><a href="/" >Use Cases</a></h1>
+        <h1 className="px-6 flex items-center text-xl text-white "><a href="/" >Roadmap</a></h1>
       </div>
 
 
