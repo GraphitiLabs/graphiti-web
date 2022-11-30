a4b49bbe05924e075e57cf192abb16a01429f636
Soham Jog <jogsoham2003@gmail.com>
2022-11-30 13:52:43 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
+import { useEffect } from 'react';
 import './App.css';
 import Mailchimp from './components/Mailchimp';
 import Navbar from './components/Navbar';
 
 
+
+
+
 //black, purple, white, gray
 function App() {
+  //make an empty useEffect
+useEffect(() => {
+
+
+}, []);
+
   return (
 
     <div >
@@ -19,31 +28,12 @@ function App() {
       
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
-
-
-        <body className='px-8 py-16  '>
-
-
-          <div className='px-40 flex flex-col items-center justify-center'>
-            <h1 className='text-6xl text-white font-bold mb-2'>Osaka is a better way of living your life</h1>
-            <p2 className='text-xl text-white font-bold mb-10'>Meet the new standard of life... eco friendly, sustainable, affordable, and socially acceptable</p2>
+          <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal'> 
+            Add zk-verifiable AI inference to your stack</h1>
+            
+            <p2 className='text-xl text-white font-bold mb-10'>The Graphiti Protocol Fast. Scalable. Intelligent</p2>
           </div>  
-  
-
-          <div className='grid gap-8 items-start justify-center'>
-            <div className='relative'>
-              <div className='absolute inset-0 bg-purple-600 rounded-lg blur'></div>
-
-              <button className='relative px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600'>
-                <span className='pr-6 text-gray-100'>Labs Release 2021</span>
-                <span className='pl-6 text-gray-100'>See what's new &rarr;</span>
-              </button>
-
-            </div>
-          </div>
-        </body>
-
-
 
       </div>
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
