f50c0e6012c059efb146549e12003df6e1459b85
Soham Jog <jogsoham2003@gmail.com>
2022-12-01 08:24:51 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -67,37 +67,41 @@ useEffect(() => {
 
             {/* SLIDE 4*/}
 
-            <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
+            <div className='snap-start bg-black w-screen h-screen flex items-center justify-between'>
 
-              <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+              <div className=' px-40 flex flex-col items-center justify-end duration-500'>
                 <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
                 Use Cases</h1>
                 
-                <div className='flex flex-row justify-center'>
-                  <div className='p-5 bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
-                    <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
-                      Insurance
-                    </h1>
-                    <h2 className='text-xl text-black font-bold mb-10 text-center'>
-                      Use Graphiti to verify the accuracy of your models and provide proof of execution to your customers
+
+
+                <div className='flex flex-row justify-between relative'>
+
+                <div className='absolute inset-0 bg-purple-600 rounded-lg blur'></div>
+
+                  <div className='m-2 p-5 bg-black w-70 h-70 rounded-lg shadow-lg relative'>
+                    
+                    <h2 className='text-xl text-gray-100 font-bold mb-10 text-center'>
+                    Leverage linear regression models to trade on DEXes
                     </h2>
                   </div>
-                  <div className='bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
-                    <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
-                      DeFi
-                    </h1>
-                    <h2 className='text-xl text-black font-bold mb-10 text-center'>
-                      Use Graphiti to verify the accuracy of your models and provide proof of execution to your customers
+
+
+                  <div className='m-2 p-5 bg-black w-70 h-70 rounded-lg shadow-lg relative'>
+
+                    <h2 className='text-xl text-gray-100 font-bold mb-10 text-center'>
+                    Use SVM prediction in your DApps
                     </h2>
                     </div>
-                  <div className='bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
-                    <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
-                      NFTs
-                    </h1>
-                    <h2 className='text-xl text-black font-bold mb-10 text-center'>
-                      Use Graphiti to verify the accuracy of your models and provide proof of execution to your customers
+
+                  <div className='m-2 p-5 bg-black w-70 h-70 rounded-lg shadow-lg relative'>
+                    
+                    <h2 className='text-xl text-gray-100 font-bold mb-10 text-center'>
+                    Assess credit risk using XGBoost in Lending Protocols
                     </h2>
                     </div>
+
+
                 </div>
 
 
@@ -129,7 +133,7 @@ useEffect(() => {
             <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
             Founders</h1>
             <div className='flex flex-row justify-center'>
-              <div className='bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
+              <div className='m-10 bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
                 <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
                   Founder 1
                 </h1>
@@ -137,7 +141,7 @@ useEffect(() => {
                   Founder 1 description
                 </h2>
               </div>
-              <div className='bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
+              <div className='m-10 bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
                 <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
                   Founder 2
                 </h1>
