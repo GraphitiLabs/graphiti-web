ab1d5422de06fc363837ed202de0c8355f59a764
Soham Jog <jogsoham2003@gmail.com>
2022-12-01 14:40:43 -0500

    purple graphiti

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,9 +9,16 @@ const NavBar = () => {
     <div>    
       <nav className="relative bg-black px-4 py-2.5 fixed w-full h-20 z-50 top-0 left-0 flex items-center justify-between">
 
+         <div>
+          <h1 className="text-purple-600  px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center">Graphiti</h1>
+         </div>
+          {
+            /*
           <div className="flex items-center">
               <h1 className='text-purple-400 text-8xl text-mono text-bold'>G</h1>
           </div>
+          */
+          }
       <div className='flex items-center justify-between'>
         <h1 className="px-6 flex items-center text-xl text-white "><a href="/" >Home</a></h1>
         <h1 className="px-6 flex items-center text-xl text-white "><a href = "/" >Team</a></h1>
