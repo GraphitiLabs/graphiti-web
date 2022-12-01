d272ef060f5c3caebf9020ff6af5d4e78a7ff58b
Soham Jog <jogsoham2003@gmail.com>
2022-12-01 15:53:19 -0500

    shrek

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import './App.css';
 import Mailchimp from './components/Mailchimp';
 import Navbar from './components/Navbar';
+import Shrek from './assets/shrek.jpeg';
 
 import FiTwitter from "react-icons/fi"
 import SiDiscord from "react-icons/si";
@@ -57,6 +58,8 @@ useEffect(() => {
           </div>  
 
       </div>
+
+      
 
             {/* SLIDE 4*/}
 
@@ -118,65 +121,105 @@ useEffect(() => {
           </div>
 
 
-      {/* SLIDE 5*/}
-
-      <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
-
-          <div className=' px-40 flex flex-col items-center justify-center duration-500'>
-            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
-            Roadmap</h1>
-          </div>  
-
-      </div>
+      
 
       {/* SLIDE 6*/}
 
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
           <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+
+            //add a picture of the member in a circle frame
+
+
             <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
-            Founders</h1>
+            Team</h1>
             <div className='flex flex-row justify-center'>
-              <div className='m-10 bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
-                <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
-                  Founder 1
+
+
+
+            <div className='relative'>
+            <div className='absolute m-8 inset-0 bg-purple-600 rounded-lg blur'></div>
+
+              <div className='relative m-10 bg-black w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
+
+              <div className='w-6/12 sm:w-4/12 px-4'>
+              <img src={Shrek} alt = "hello" className='shadow rounded-full max-w-full h-auto align-middle border-none"'/>
+              </div>
+
+                <h1 className='text-4xl text-gray-100 font-bold mb-2 break-normal text-center'>
+                  Ketan Jog
                 </h1>
-                <h2 className='text-xl text-black font-bold mb-10 text-center'>
-                  Founder 1 description
+                <h2 className='px-5 text-xl text-gray-100 font-bold mb-5 text-center'>
+                  Founder 1 description. I like a cup of coffee every morning. I love to read the newspaper every morning. I'm kind of a weirdo. I make funny faces in front of the mirror.
                 </h2>
               </div>
-              <div className='m-10 bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
-                <h1 className='text-4xl text-black font-bold mb-2 break-normal text-center'>
-                  Founder 2
+              </div>
+
+
+
+              <div className='relative'>
+              <div className='absolute m-8 inset-0 bg-purple-600 rounded-lg blur'></div>
+
+              <div className='relative m-10 bg-black w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
+
+              <div className='w-6/12 sm:w-4/12 px-4'>
+              <img src={Shrek} alt = "hello" className='shadow rounded-full max-w-full h-auto align-middle border-none"'/>
+              </div>
+
+                <h1 className='text-4xl text-gray-100 font-bold mb-2 break-normal text-center'>
+                  Andrew Kuo
                 </h1>
-                <h2 className='text-xl text-black font-bold mb-10 text-center'>
+                <h2 className='text-xl text-gray-100 font-bold mb-10 text-center'>
                   Founder 2 description
                 </h2>
-                </div>
-                </div>
+              </div>
+              </div>
+
+
+              <div className='relative'>
+              <div className='absolute m-8 inset-0 bg-purple-600 rounded-lg blur'></div>
+
+
+              <div className='relative m-10 bg-black w-96 h-96 rounded-lg shadow-lg flex flex-col items-center justify-center'>
+
+              <div className='w-6/12 sm:w-4/12 px-4'>
+              <img src={Shrek} alt = "hello" className='shadow rounded-full max-w-full h-auto align-middle border-none"'/>
+              </div>
+
+                <h1 className='text-4xl text-gray-100 font-bold mb-2 break-normal text-center'>
+                  Maryn Man
+                </h1>
+                <h2 className='text-xl text-gray-100 font-bold mb-10 text-center'>
+                  Founder 2 description
+                </h2>
+              </div>
+              </div>
+              
+            </div>
 
           </div>  
 
       </div>
 
 
-      {/* SLIDE 7*/}
+
+      {/* SLIDE 5*/}
 
       <div className='snap-start bg-black w-screen h-screen flex items-center justify-center '>
 
-          <div className=' px-40 flex flex-col items-center justify-center duration-500'>
-            <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
-            Advisors</h1>
-          </div>  
+      <div className=' px-40 flex flex-col items-center justify-center duration-500'>
+        <h1 className='px-20 py-10 text-6xl text-white font-bold mb-2 break-normal text-center'> 
+        Roadmap</h1>
+      </div>  
 
       </div>
-
 
       {/* FORM SLIDE */}
 
       <div className='snap-start bg-black w-screen h-screen items-center'>
 
-      <h1 className='px-60 py-10 text-4xl text-white font-bold mb-2 text-center'>Get the latest updates on what we are building! Whitepaper coming soon…stay tuned!</h1>
+      <h1 className='px-60 py-10 text-4xl text-white font-bold mb-2 text-center'>Get the latest updates on what we are building! Whitepaper coming soon… stay tuned!</h1>
 
       <div className='flex items-center justify-center '>
       <div className='relative'>
@@ -185,6 +228,15 @@ useEffect(() => {
             <Mailchimp/>
       </div>
       </div>
+      //add a footer that has a copyright symbol and the year
+      <footer>
+        <div >
+          <h1 className='mt-60 text-white text-center text-l'>© 2022 Graphiti Inc. All rights reserved.</h1>
+        </div>
+      </footer>
+
+
+
       </div>
 
 
